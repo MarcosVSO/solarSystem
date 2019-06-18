52bba2b2750843a9b6c2f315a8093f40b4956645
marcosVSO <marcosv.soliver@gmail.com>
2019-06-18 12:54:38 -0300

    Translation fixed, I think...

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,5 @@
-var canvasW = 1920;
-canvasH = 1080;
-let v1;
+var canvasW = 3000;
+canvasH = 2000;
 let angle = 0;
 let sol;
 let terra;
@@ -8,29 +7,35 @@ let terra;
 function setup() {
   createCanvas(canvasW,canvasH);
   angleMode(DEGREES);
-  sol = new Planet(canvasW/2,canvasH/2,600,"#ff0000");
+  translate(canvasW/2,canvasH/2);
+  sol = new Planet(0,0,1000,"#ff0000");
   dSolDireita = sol.x - (sol.diameter/2);
   dSolEsquerda = sol.x + (sol.diameter/2);
-  terra = new Planet(dSolDireita - 150000000/1000000,sol.y,sol.diameter * 0.009159292,"#4286f4");
+  terra = new Planet(dSolDireita - 150000000/1000000,sol.y,sol.diameter * 0.009159292,"#4286f4",(360/365));
 }
 
 function draw() {
   background(230);
+  translate(canvasW/2,canvasH/2);
   sol.display();
-  translate(sol.x,sol.y)
-  //rotate(angle,[dSolDireita,147098290/1000000 + sol.y + (sol.diameter/2)]);
+  rotate(angle);
   terra.display();
-  //terra.translationLine();
-  //angle = angle + 1;
+  angle = angle + 1 * (360/365);
+  print(angle);
+  if(angle >= 360){
+    angle = 0;
+  }
 
 }
 
 class Planet{
-  constructor(x,y,diameter,color){
+  
+    constructor(x,y,diameter,color,velocity){
     this.x = x;
     this.y = y;
     this.diameter = diameter;
     this.color=color;
+    this.veloctity = velocity;
   }
 
   display(){
@@ -38,12 +43,6 @@ class Planet{
     noStroke();
     ellipse(this.x,this.y,this.diameter,this.diameter);
   }
-
-  translationLine(){
-    ellipse(this.x,this.y,this.x + dSolDireita,this.y + dSolDireita);
-  }
-
-  
 }
 
 
